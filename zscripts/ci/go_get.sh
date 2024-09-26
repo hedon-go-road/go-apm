6d24293869f3c6576b73c2584e5f2c9650feddde
hedon954 <171725713@qq.com>
2024-09-26 10:52:46 +0800

    fix(ci): support workspace

--- zscripts/ci/go_get.sh
+++ zscripts/ci/go_get.sh
@@ -3,11 +3,11 @@
 set -e
 
 # Array of service directories
-services=("dogalarm" "dogapm" "ordersvc" "skusvc" "usrsvc")
+services=("dogalarm" "dogapm" "ordersvc" "skusvc" "usrsvc" "protos")
 
 # Loop through each service and run tests
 for service in "${services[@]}"; do
     cd "$service"
-    go test -race ./...
+    go get
     cd ..
 done
--- zscripts/ci/go_test_cov.sh
+++ zscripts/ci/go_test_cov.sh
@@ -8,6 +8,8 @@ services=("dogalarm" "dogapm" "ordersvc" "skusvc" "usrsvc")
 # Loop through each service and run tests
 for service in "${services[@]}"; do
     cd "$service"
-    go test -race ./...
+    go test -v -race -covermode=atomic -coverpkg=./... -coverprofile=coverage.out ./...
+    go tool cover -func=coverage.out
+    mv coverage.out ../coverage_${service}.out
     cd ..
 done
