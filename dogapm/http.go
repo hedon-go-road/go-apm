cd998d90bc12f30d493f535b0e962d03284fc231
hedon954 <171725713@qq.com>
2024-10-15 11:47:00 +0800

    feat(http): wrap http with otel

--- dogapm/http.go
+++ dogapm/http.go
@@ -5,11 +5,21 @@ import (
 	"log"
 	"net/http"
 	"time"
+
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/attribute"
+	"go.opentelemetry.io/otel/propagation"
+	"go.opentelemetry.io/otel/trace"
+)
+
+const (
+	httpTracerName = "dogapm/http"
 )
 
 type HTTPServer struct {
 	mux *http.ServeMux
 	*http.Server
+	tracer trace.Tracer
 }
 
 func NewHTTPServer(addr string) *HTTPServer {
@@ -21,6 +31,7 @@ func NewHTTPServer(addr string) *HTTPServer {
 			Handler:           mux,
 			ReadHeaderTimeout: 30 * time.Second, //nolint:mnd
 		},
+		tracer: otel.Tracer(httpTracerName),
 	}
 	globalStarters = append(globalStarters, s)
 	globalClosers = append(globalClosers, s)
@@ -36,11 +47,17 @@ func (s *HTTPServer) Close() {
 }
 
 func (s *HTTPServer) Handle(pattern string, handler http.Handler) {
-	s.mux.Handle(pattern, handler)
+	s.mux.Handle(pattern, &traceHandler{
+		handler: handler,
+		tracer:  s.tracer,
+	})
 }
 
 func (s *HTTPServer) HandleFunc(pattern string, handler func(http.ResponseWriter, *http.Request)) {
-	s.mux.HandleFunc(pattern, handler)
+	s.mux.Handle(pattern, &traceHandler{
+		handler: http.HandlerFunc(handler),
+		tracer:  s.tracer,
+	})
 }
 
 func (s *HTTPServer) Start() {
@@ -56,4 +73,63 @@ func (s *HTTPServer) Stop() {
 	if err := s.Server.Shutdown(context.Background()); err != nil {
 		log.Fatal("Error stopping server:", err)
 	}
+}
+
+type traceHandler struct {
+	handler http.Handler
+	tracer  trace.Tracer
+}
+
+func (h *traceHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	if h.tracer == nil {
+		h.handler.ServeHTTP(w, r)
+		return
+	}
+	ctx := r.Context()
+	ctx = otel.GetTextMapPropagator().Extract(ctx, propagation.HeaderCarrier(r.Header))
+	ctx, span := h.tracer.Start(ctx, "HTTP "+r.Method+" "+r.URL.Path)
+	defer span.End()
+	r = r.Clone(ctx)
+	respWrapper := &responseWrapper{ResponseWriter: w}
+
+	start := time.Now()
+
+	func() {
+		// panic recover
+		defer func() {
+			if err := recover(); err != nil {
+				// TODO: log and trace
+				log.Printf("Panic: %v", err)
+				http.Error(respWrapper, "Internal Server Error", http.StatusInternalServerError)
+			}
+		}()
+
+		// handle request
+		h.handler.ServeHTTP(respWrapper, r)
+	}()
+
+	if respWrapper.status == 0 {
+		respWrapper.status = http.StatusOK
+	}
+	elapsed := time.Since(start)
+	span.SetAttributes(
+		attribute.KeyValue{
+			Key:   "http.status_code",
+			Value: attribute.IntValue(respWrapper.status),
+		},
+		attribute.KeyValue{
+			Key:   "http.duration_ms",
+			Value: attribute.Int64Value(elapsed.Milliseconds()),
+		},
+	)
+}
+
+type responseWrapper struct {
+	http.ResponseWriter
+	status int
+}
+
+func (w *responseWrapper) WriteHeader(status int) {
+	w.status = status
+	w.ResponseWriter.WriteHeader(status)
 }
