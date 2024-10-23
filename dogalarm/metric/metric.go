7d76449fc8884c475f7ef57c3d3b10d77ac3cc9d
hedon954 <171725713@qq.com>
2024-10-23 20:52:32 +0800

    feat: add metric for dogalarm

--- dogalarm/metric/metric.go
+++ dogalarm/metric/metric.go
@@ -2,6 +2,15 @@ package metric
 
 import "github.com/prometheus/client_golang/prometheus"
 
+var (
+	DropAlarmCounter = prometheus.NewCounterVec(prometheus.CounterOpts{
+		Name: "dogalarm_drop_alarm_total",
+		Help: "The total number of dropped alarms",
+	}, []string{"app"})
+)
+
 func All() []prometheus.Collector {
-	return []prometheus.Collector{}
+	return []prometheus.Collector{
+		DropAlarmCounter,
+	}
 }
